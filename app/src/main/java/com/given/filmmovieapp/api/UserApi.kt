e201978ef33723d106d21307742e7a920b4434b4
kevinbomb <kevinwomb@gmail.com>
2022-12-06 12:14:11 +0700

    volley user pt.1

---
+++ app/src/main/java/com/given/filmmovieapp/api/UserApi.kt
@@ -0,0 +1,13 @@
+package com.given.filmmovieapp.api
+
+class UserApi {
+    companion object{
+        val BASE_URL="http://192.168.196.147/ci4/ci4/public/"
+
+        val GET_ALL_URL = BASE_URL + "user/"
+        val GET_BY_ID_URL = BASE_URL + "user/"
+        val ADD_URL = BASE_URL + "user"
+        val UPDATE_URL = BASE_URL + "user/"
+        val DELETE_URL = BASE_URL + "user/"
+    }
+}
---
+++ app/src/main/java/com/given/filmmovieapp/models/User.kt
@@ -0,0 +1,5 @@
+package com.given.filmmovieapp.models
+
+class User (var email: String,var username: String,var password: String, var tangalLahir: String, var noTelepon: String) {
+    var id: Long? = null
+}
